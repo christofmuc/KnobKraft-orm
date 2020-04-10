6b596355447848dae5e3c49b722db02976766424
cruc <christof.ruch@gmx.de>
2020-04-10 14:03:18 +0200

    Making the code compile within the MidiKraft Environment

--- synths/roland-mks80/MKS80.h
+++ synths/roland-mks80/MKS80.h
@@ -6,69 +6,72 @@
 #include "EditBufferCapability.h"
 #include "HandshakeLoadingCapability.h"
 #include "SoundExpanderCapability.h"
-#include "SupportedByBCR2000.h"
+//#include "SupportedByBCR2000.h"
 
 #include "MKS80_Parameter.h"
 
-class MKS80 : public Synth, public EditBufferCapability, public HandshakeLoadingCapability, public SoundExpanderCapability, public SupportedByBCR2000 {
-public:
-	MKS80();
+namespace midikraft {
 
-	virtual int numberOfBanks() const override;
-	virtual int numberOfPatches() const override;
-	virtual std::string friendlyBankName(MidiBankNumber bankNo) const override;
+	class MKS80 : public Synth, public EditBufferCapability, public HandshakeLoadingCapability, public SoundExpanderCapability /*, public SupportedByBCR2000 */ {
+	public:
+		MKS80();
 
-	virtual std::shared_ptr<Patch> patchFromPatchData(const Synth::PatchData &data, std::string const &name, MidiProgramNumber place) const override;
+		virtual int numberOfBanks() const override;
+		virtual int numberOfPatches() const override;
+		virtual std::string friendlyBankName(MidiBankNumber bankNo) const override;
 
-	virtual bool isOwnSysex(MidiMessage const &message) const override;
+		virtual std::shared_ptr<DataFile> patchFromPatchData(const Synth::PatchData &data, std::string const &name, MidiProgramNumber place) const override;
 
-	virtual MidiMessage deviceDetect(int channel) override;
-	virtual int deviceDetectSleepMS() override;
-	virtual MidiChannel channelIfValidDeviceResponse(const MidiMessage &message) override;
-	virtual bool needsChannelSpecificDetection() override;
-	virtual bool endDeviceDetect(MidiMessage &endMessageOut) const override;
+		virtual bool isOwnSysex(MidiMessage const &message) const override;
 
-	virtual std::string getName() const override;
+		virtual MidiMessage deviceDetect(int channel) override;
+		virtual int deviceDetectSleepMS() override;
+		virtual MidiChannel channelIfValidDeviceResponse(const MidiMessage &message) override;
+		virtual bool needsChannelSpecificDetection() override;
+		virtual bool endDeviceDetect(MidiMessage &endMessageOut) const override;
 
-	// Edit Buffer Capability
-	virtual MidiMessage requestEditBufferDump() override;
-	virtual bool isEditBufferDump(const MidiMessage& message) const override;
-	virtual std::shared_ptr<Patch> patchFromSysex(const MidiMessage& message) const override;
-	virtual std::vector<MidiMessage> patchToSysex(const Patch &patch) const override;
-	virtual MidiMessage saveEditBufferToProgram(int programNumber) override;
+		virtual std::string getName() const override;
 
-	// HandshakeLoadingCapability
-	virtual std::shared_ptr<ProtocolState> createStateObject() override;
-	virtual void startDownload(SafeMidiOutput *output, std::shared_ptr<ProtocolState> saveState) override;
-	virtual bool isNextMessage(MidiMessage const &message, std::vector<MidiMessage> &answer, std::shared_ptr<ProtocolState> state) override;
+		// Edit Buffer Capability
+		virtual MidiMessage requestEditBufferDump() override;
+		virtual bool isEditBufferDump(const MidiMessage& message) const override;
+		virtual std::shared_ptr<Patch> patchFromSysex(const MidiMessage& message) const override;
+		virtual std::vector<MidiMessage> patchToSysex(const Patch &patch) const override;
+		virtual MidiMessage saveEditBufferToProgram(int programNumber) override;
 
-	// Sound expander capability
-	virtual bool canChangeInputChannel() const override;
-	virtual void changeInputChannel(MidiController *controller, MidiChannel channel) override;
-	virtual MidiChannel getInputChannel() const override;
-	virtual bool hasMidiControl() const override;
-	virtual bool isMidiControlOn() const override;
-	virtual void setMidiControl(MidiController *controller, bool isOn) override;
+		// HandshakeLoadingCapability
+		virtual std::shared_ptr<ProtocolState> createStateObject() override;
+		virtual void startDownload(std::shared_ptr<SafeMidiOutput> output, std::shared_ptr<ProtocolState> saveState) override;
+		virtual bool isNextMessage(MidiMessage const &message, std::vector<MidiMessage> &answer, std::shared_ptr<ProtocolState> state) override;
+
+		// Sound expander capability
+		virtual bool canChangeInputChannel() const override;
+		virtual void changeInputChannel(MidiController *controller, MidiChannel channel, std::function<void()> onFinished) override;
+		virtual MidiChannel getInputChannel() const override;
+		virtual bool hasMidiControl() const override;
+		virtual bool isMidiControlOn() const override;
+		virtual void setMidiControl(MidiController *controller, bool isOn) override;
 
 
-	// Override for funky formats
-	TPatchVector loadSysex(std::vector<MidiMessage> const &sysexMessages) override;
+		// Override for funky formats
+		TPatchVector loadSysex(std::vector<MidiMessage> const &sysexMessages) override;
 
-	// SupportedByBCR2000
-	std::vector<std::string> presetNames() override;
+		// SupportedByBCR2000
+		/*std::vector<std::string> presetNames() override;
+		void setupBCR2000(MidiController *controller, BCR2000 &bcr, SimpleLogger *logger) override;
+		void syncDumpToBCR(MidiProgramNumber programNumber, MidiController *controller, BCR2000 &bcr, SimpleLogger *logger) override;
+		void setupBCR2000View(BCR2000_Component &view) override;
+		void setupBCR2000Values(BCR2000_Component &view, Patch *patch) override;*/
 
-	void setupBCR2000(MidiController *controller, BCR2000 &bcr, SimpleLogger *logger) override;
-	void syncDumpToBCR(MidiProgramNumber programNumber, MidiController *controller, BCR2000 &bcr, SimpleLogger *logger) override;
-	void setupBCR2000View(BCR2000_Component &view) override;
-	void setupBCR2000Values(BCR2000_Component &view, Patch *patch) override;
+	private:
+		std::string presetName();
 
-private:
-	std::string presetName();
+		static uint8 rolandChecksum(std::vector<uint8>::iterator start, std::vector<uint8>::iterator end);
 
-	static uint8 rolandChecksum(std::vector<uint8>::iterator start, std::vector<uint8>::iterator end);
+		MidiMessage buildHandshakingMessage(MKS80_Operation_Code code) const;
+		MidiMessage buildHandshakingMessage(MKS80_Operation_Code code, MidiChannel channel) const;
 
-	MidiMessage buildHandshakingMessage(MKS80_Operation_Code code) const;
-	MidiMessage buildHandshakingMessage(MKS80_Operation_Code code, MidiChannel channel) const;
+		MKS80_Operation_Code getSysexOperationCode(MidiMessage const &message) const;
+	};
 
-	MKS80_Operation_Code getSysexOperationCode(MidiMessage const &message) const;
-};
+}
--- synths/roland-mks80/MKS80_Patch.h
+++ synths/roland-mks80/MKS80_Patch.h
@@ -1,52 +1,58 @@
 #pragma once
 
 #include "Patch.h"
+#include "MidiProgramNumber.h"
 
-class MKS80_PatchNumber : public PatchNumber {
-public:
-	MKS80_PatchNumber(MidiProgramNumber patchnumber);
-	MKS80_PatchNumber(int bank, int patch);
+namespace midikraft {
 
-	virtual std::string friendlyName() const override;
-};
+	class MKS80_PatchNumber : public PatchNumber {
+	public:
+		MKS80_PatchNumber(MidiProgramNumber patchnumber);
+		MKS80_PatchNumber(int bank, int patch);
 
-class MKS80_Patch : public Patch {
-public:
-	enum class APR_Section {
-		PATCH_UPPER = (0b00110000 | 0b00000001),
-		PATCH_LOWER = (0b00110000 | 0b00000010),
-		TONE_UPPER = (0b00100000 | 0b00000001),
-		TONE_LOWER = (0b00100000 | 0b00000010),
+		virtual std::string friendlyName() const override;
 	};
 
-	MKS80_Patch(std::shared_ptr<MKS80_PatchNumber> patchNumber, std::map<APR_Section, std::vector<uint8>> const &data);
-	MKS80_Patch(MidiProgramNumber patchNumber, Synth::PatchData const &data);
+	class MKS80_Patch : public Patch {
+	public:
+		enum DataFileType {
+			DF_MKS80_PATCH = 0
+		};
+		enum class APR_Section {
+			PATCH_UPPER = (0b00110000 | 0b00000001),
+			PATCH_LOWER = (0b00110000 | 0b00000010),
+			TONE_UPPER = (0b00100000 | 0b00000001),
+			TONE_LOWER = (0b00100000 | 0b00000010),
+		};
 
-	virtual std::string patchName() const override;
-	virtual void setName(std::string const &name) override;
+		MKS80_Patch(std::shared_ptr<MKS80_PatchNumber> patchNumber, std::map<APR_Section, std::vector<uint8>> const &data);
+		MKS80_Patch(MidiProgramNumber patchNumber, Synth::PatchData const &data);
 
-	virtual std::shared_ptr<PatchNumber> patchNumber() const override;
-	virtual void setPatchNumber(MidiProgramNumber patchNumber) override;
+		virtual std::string patchName() const override;
+		virtual void setName(std::string const &name) override;
 
-	virtual int value(SynthParameterDefinition const &param) const override;
-	virtual SynthParameterDefinition const & paramBySysexIndex(int sysexIndex) const override;
-	virtual std::vector<std::string> warnings() override;
+		virtual std::shared_ptr<PatchNumber> patchNumber() const override;
+		virtual void setPatchNumber(MidiProgramNumber patchNumber) override;
 
-	virtual std::vector<SynthParameterDefinition *> allParameterDefinitions() override;
+		int value(SynthParameterDefinition const &param) const;
+		SynthParameterDefinition const & paramBySysexIndex(int sysexIndex) const;
 
-	// Support APR format creation
-	MidiMessage buildAPRMessage(APR_Section section, MidiChannel channel) const;
+		virtual std::vector<std::shared_ptr<SynthParameterDefinition>> allParameterDefinitions() override;
 
-	//! For validating the enum class before casting
-	static bool isValidAPRSection(int section);
+		// Support APR format creation
+		MidiMessage buildAPRMessage(APR_Section section, MidiChannel channel) const;
 
-	//! For the funky DAT format
-	static std::vector<uint8> toneFromDat(std::vector<uint8> const &dat);
-	static std::vector<uint8> patchesFromDat(std::vector<uint8> const &dat);
+		//! For validating the enum class before casting
+		static bool isValidAPRSection(int section);
 
-private:
-	void copyDataSection(std::map<APR_Section, std::vector<uint8>> const &data, std::vector<uint8> &result, MKS80_Patch::APR_Section section, int expectedLength);
+		//! For the funky DAT format
+		static std::vector<uint8> toneFromDat(std::vector<uint8> const &dat);
+		static std::vector<uint8> patchesFromDat(std::vector<uint8> const &dat);
 
-	std::shared_ptr<MKS80_PatchNumber> patchNumber_;
-};
+	private:
+		void copyDataSection(std::map<APR_Section, std::vector<uint8>> const &data, std::vector<uint8> &result, MKS80_Patch::APR_Section section, int expectedLength);
 
+		std::shared_ptr<MKS80_PatchNumber> patchNumber_;
+	};
+
+}
