01407660177a91ebb6e29aba1af7062d15a69eba
cruc <christof.ruch@gmx.de>
2020-04-14 22:46:03 +0200

    Fix MIDI channel display to be properly one based

--- synths/oberheim-matrix1000/Matrix1000.cpp
+++ synths/oberheim-matrix1000/Matrix1000.cpp
@@ -48,14 +48,15 @@ namespace midikraft {
 	struct Matrix1000GlobalSettingDefinition {
 		int sysexIndex;
 		TypedNamedValue typedNamedValue;
-		bool isOnesComplement;
+		bool isTwosComplement;
+		int displayOffset;
 	};
 
 	// Table of global settings. What I left out was the "group enabled" array for one bit per patch to specify if it is deemed to be group-worthy.
 	std::vector<Matrix1000GlobalSettingDefinition> kMatrix1000GlobalSettings = {
 		{ 34, { "Master Transpose", "Tuning", Value(), ValueType::Integer, -24, 24 }, true },
 		{ 8, { "Master Tune", "Tuning", Value(), ValueType::Integer, -32, 32 }, true },
-		{ 11, { "MIDI Basic Channel", "MIDI", Value(), ValueType::Integer, 0, 15 }, 1 /* Make it one based */},
+		{ 11, { "MIDI Basic Channel", "MIDI", Value(), ValueType::Integer, 1, 16 }, false, 1 /* Make it one based */},
 		{ 12, { "MIDI OMNI Mode Enable", "MIDI", Value(), ValueType::Bool, 0, 1 } },
 		{ 13, { "MIDI Controllers enable", "MIDI", Value(), ValueType::Bool, 0, 1 } },
 		{ 14, { "MIDI Patch Changes Enable", "MIDI", Value(), ValueType::Bool, 0, 1 } },
@@ -599,8 +600,8 @@ namespace midikraft {
 		if (settingsArray.size() == 172) {
 			for (size_t i = 0; i < kMatrix1000GlobalSettings.size(); i++) {
 				if (i < settingsArray.size()) {
-					int intValue = settingsArray[kMatrix1000GlobalSettings[i].sysexIndex];
-					if (kMatrix1000GlobalSettings[i].isOnesComplement) {
+					int intValue = settingsArray[kMatrix1000GlobalSettings[i].sysexIndex] + kMatrix1000GlobalSettings[i].displayOffset;
+					if (kMatrix1000GlobalSettings[i].isTwosComplement) {
 						// Very special code that only works because there are just two fields in the global settings that need it
 						// Master transpose and Master tuning
 						if (intValue > 127) {
