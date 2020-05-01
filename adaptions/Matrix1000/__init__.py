f83385d99d302f18ac1419aac60d7794ec98bc2b
cruc <christof.ruch@gmx.de>
2020-05-01 17:55:32 +0200

    Bugfix  - don't send numbers higher than 0x64 to the M1000, else you will get no reply

--- adaptions/Matrix1000/__init__.py
+++ adaptions/Matrix1000/__init__.py
@@ -48,7 +48,9 @@ def isEditBufferDump(message):
 	return isSingleProgramDump(message)
 
 def createProgramDumpRequest(channel, patchNo):
-	return [0xf0, 0x10, 0x06, 0x04, 1, patchNo, 0xf7]
+	# Todo - I need to create a bank select upfront
+	program = patchNo % 100
+	return [0xf0, 0x10, 0x06, 0x04, 1, program, 0xf7]
 
 def isSingleProgramDump(message):
 	return (len(message) > 3 
