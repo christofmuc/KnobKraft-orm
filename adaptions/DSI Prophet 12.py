e8c75cf1f8a7fca9b5921019e91f74a34727cf15
cruc <christof.ruch@gmx.de>
2020-05-09 17:11:50 +0200

    A bit of python reformatting making things nicer here.

--- adaptions/DSI Prophet 12.py
+++ adaptions/DSI Prophet 12.py
@@ -6,92 +6,105 @@
 
 prophet12_ID = 0b00101010  # See Page 82 of the prophet 12 manual
 
+
 def name():
-	return "DSI Prophet 12"
+    return "DSI Prophet 12"
+
 
 def createDeviceDetectMessage(channel):
-	# This is a sysex generic device detect message
-	return [0xf0, 0x7e, channel, 0x06, 0x01, 0xf7]
+    # This is a sysex generic device detect message
+    return [0xf0, 0x7e, channel, 0x06, 0x01, 0xf7]
+
 
 def deviceDetectWaitMilliseconds():
-	return 100
+    return 100
+
 
 def needsChannelSpecificDetection():
-	return False
+    return False
 
-def channelIfValidDeviceResponse(message):	
-	if (len(message) > 9 
-		and message[0] == 0xf0  # Sysex
-		and message[1] == 0x7e  # Non-realtime
-		and message[3] == 0x06  # Device request
-		and message[4] == 0x02  # Device request reply
-		and message[5] == 0x01  # Sequential / Dave Smith Instruments
-		and message[6] == prophet12_ID  
-		and message[7] == 0x01  # Family MS is 1
-		and message[8] == 0x00  # Family member 
-		and message[9] == 0x00  # Family member 
-		):
-		# Extract the current MIDI channel from index 2 of the message
-		return message[2]
-	return -1
+
+def channelIfValidDeviceResponse(message):
+    if (len(message) > 9
+            and message[0] == 0xf0  # Sysex
+            and message[1] == 0x7e  # Non-realtime
+            and message[3] == 0x06  # Device request
+            and message[4] == 0x02  # Device request reply
+            and message[5] == 0x01  # Sequential / Dave Smith Instruments
+            and message[6] == prophet12_ID
+            and message[7] == 0x01  # Family MS is 1
+            and message[8] == 0x00  # Family member
+            and message[9] == 0x00):  # Family member
+        # Extract the current MIDI channel from index 2 of the message
+        return message[2]
+    return -1
+
 
 def createEditBufferRequest(channel):
-	return [0xf0, 0x01, prophet12_ID, 0b00000110, 0xf7]
+    return [0xf0, 0x01, prophet12_ID, 0b00000110, 0xf7]
 
-def isEditBufferDump(message):	
-	return (len(message) > 3 
-		and message[0] == 0xf0 
-		and message[1] == 0x01   # Sequential
-		and message[2] == prophet12_ID
-		and message[3] == 0b00000011  # Edit Buffer Data
-		)
+
+def isEditBufferDump(message):
+    return (len(message) > 3
+            and message[0] == 0xf0
+            and message[1] == 0x01  # Sequential
+            and message[2] == prophet12_ID
+            and message[3] == 0b00000011  # Edit Buffer Data
+            )
+
 
 def numberOfBanks():
-	return 8
+    return 8
+
 
 def numberOfPatchesPerBank():
-	return 128
+    return 128
+
 
 def createProgramDumpRequest(channel, patchNo):
-	bank = patchNo // numberOfPatchesPerBank()
-	program = patchNo % numberOfPatchesPerBank()
-	return [0xf0, 0x01, prophet12_ID, 0b00000101, bank, program, 0xf7]
+    bank = patchNo // numberOfPatchesPerBank()
+    program = patchNo % numberOfPatchesPerBank()
+    return [0xf0, 0x01, prophet12_ID, 0b00000101, bank, program, 0xf7]
+
 
 def isSingleProgramDump(message):
-	return (len(message) > 3 
-		and message[0] == 0xf0 
-		and message[1] == 0x01   # Sequential
-		and message[2] == prophet12_ID
-		and message[3] == 0b00000010  # Program Data
-		)
+    return (len(message) > 3
+            and message[0] == 0xf0
+            and message[1] == 0x01  # Sequential
+            and message[2] == prophet12_ID
+            and message[3] == 0b00000010  # Program Data
+            )
+
 
 def nameFromDump(message):
-	dataBlock = []
-	if isSingleProgramDump(message):		
-		dataBlock = message[6:-1]
-	elif isEditBufferDump(message):
-		dataBlock = message[4:-1]
-	if len(dataBlock) > 0:
-		patchData = unescapeSysex(dataBlock)
-		return ''.join([chr(x) for x in patchData[914:931]])
-	return "Invalid"
+    dataBlock = []
+    if isSingleProgramDump(message):
+        dataBlock = message[6:-1]
+    elif isEditBufferDump(message):
+        dataBlock = message[4:-1]
+    if len(dataBlock) > 0:
+        patchData = unescapeSysex(dataBlock)
+        return ''.join([chr(x) for x in patchData[914:931]])
+    return "Invalid"
+
 
 def convertToEditBuffer(channel, message):
-	if isEditBufferDump(message):
-		return message
-	elif isSingleProgramDump(message):
-		# Have to strip out bank and program, and set command to edit buffer dump
-		return message[0:3] + [0b00000011] + message[6:]
-	raise "Neither edit buffer nor program dump - can't be converted"
+    if isEditBufferDump(message):
+        return message
+    elif isSingleProgramDump(message):
+        # Have to strip out bank and program, and set command to edit buffer dump
+        return message[0:3] + [0b00000011] + message[6:]
+    raise Exception("Neither edit buffer nor program dump - can't be converted")
+
 
 def unescapeSysex(sysex):
-	result = []
-	dataIndex = 0
-	while (dataIndex < len(sysex)):
-		msbits = sysex[dataIndex]
-		dataIndex += 1
-		for i in range(7):
-			if dataIndex < len(sysex):
-				result.append(sysex[dataIndex] | ((msbits & (1 << i)) << (7 - i)))
-			dataIndex += 1
-	return result
+    result = []
+    dataIndex = 0
+    while dataIndex < len(sysex):
+        msbits = sysex[dataIndex]
+        dataIndex += 1
+        for i in range(7):
+            if dataIndex < len(sysex):
+                result.append(sysex[dataIndex] | ((msbits & (1 << i)) << (7 - i)))
+            dataIndex += 1
+    return result
--- adaptions/DSI Prophet 6.py
+++ adaptions/DSI Prophet 6.py
@@ -4,92 +4,105 @@
 #   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
 #
 
+
 def name():
-	return "DSI Prophet 6"
+    return "DSI Prophet 6"
+
 
 def createDeviceDetectMessage(channel):
-	# This is a sysex generic device detect message
-	return [0xf0, 0x7e, channel, 0x06, 0x01, 0xf7]
+    # This is a sysex generic device detect message
+    return [0xf0, 0x7e, channel, 0x06, 0x01, 0xf7]
+
 
 def deviceDetectWaitMilliseconds():
-	return 100
+    return 100
+
 
 def needsChannelSpecificDetection():
-	return False
+    return False
 
-def channelIfValidDeviceResponse(message):	
-	if (len(message) > 9 
-		and message[0] == 0xf0  # Sysex
-		and message[1] == 0x7e  # Non-realtime
-		and message[3] == 0x06  # Device request
-		and message[4] == 0x02  # Device request reply
-		and message[5] == 0x01  # Sequential / Dave Smith Instruments
-		and message[6] == 0b00101101  # Prophet 6
-		and message[7] == 0x01  # Familiry MS
-		and message[8] == 0x00  # Family 
-		and message[9] == 0x00  # Family 
-		):
-		# Extract the current MIDI channel from index 2 of the message
-		return message[2]
-	return -1
+
+def channelIfValidDeviceResponse(message):
+    if (len(message) > 9
+            and message[0] == 0xf0  # Sysex
+            and message[1] == 0x7e  # Non-realtime
+            and message[3] == 0x06  # Device request
+            and message[4] == 0x02  # Device request reply
+            and message[5] == 0x01  # Sequential / Dave Smith Instruments
+            and message[6] == 0b00101101  # Prophet 6
+            and message[7] == 0x01  # Familiry MS
+            and message[8] == 0x00  # Family
+            and message[9] == 0x00):  # Family
+        # Extract the current MIDI channel from index 2 of the message
+        return message[2]
+    return -1
+
 
 def createEditBufferRequest(channel):
-	return [0xf0, 0x01, 0b00101101, 0b00000110, 0xf7]
+    return [0xf0, 0x01, 0b00101101, 0b00000110, 0xf7]
 
-def isEditBufferDump(message):	
-	return (len(message) > 3 
-		and message[0] == 0xf0 
-		and message[1] == 0x01   # Sequential
-		and message[2] == 0b00101101  # Prophet 6
-		and message[3] == 0b00000011  # Edit Buffer Data
-		)
+
+def isEditBufferDump(message):
+    return (len(message) > 3
+            and message[0] == 0xf0
+            and message[1] == 0x01  # Sequential
+            and message[2] == 0b00101101  # Prophet 6
+            and message[3] == 0b00000011  # Edit Buffer Data
+            )
+
 
 def numberOfBanks():
-	return 10
+    return 10
+
 
 def numberOfPatchesPerBank():
-	return 100
+    return 100
+
 
 def createProgramDumpRequest(channel, patchNo):
-	bank = patchNo // numberOfPatchesPerBank()
-	program = patchNo % numberOfPatchesPerBank()
-	return [0xf0, 0x01, 0b00101101, 0b00000101, bank, program, 0xf7]
+    bank = patchNo // numberOfPatchesPerBank()
+    program = patchNo % numberOfPatchesPerBank()
+    return [0xf0, 0x01, 0b00101101, 0b00000101, bank, program, 0xf7]
+
 
 def isSingleProgramDump(message):
-	return (len(message) > 3 
-		and message[0] == 0xf0 
-		and message[1] == 0x01   # Sequential
-		and message[2] == 0b00101101  # Prophet 6
-		and message[3] == 0b00000010  # Program Data
-		)
+    return (len(message) > 3
+            and message[0] == 0xf0
+            and message[1] == 0x01  # Sequential
+            and message[2] == 0b00101101  # Prophet 6
+            and message[3] == 0b00000010  # Program Data
+            )
+
 
 def nameFromDump(message):
-	dataBlock = []
-	if isSingleProgramDump(message):		
-		dataBlock = message[6:-1]
-	elif isEditBufferDump(message):
-		dataBlock = message[4:-1]
-	if len(dataBlock) > 0:
-		patchData = unescapeSysex(dataBlock)		
-		return ''.join([chr(x) for x in patchData[107:127]])
-	return "Invalid"
+    dataBlock = []
+    if isSingleProgramDump(message):
+        dataBlock = message[6:-1]
+    elif isEditBufferDump(message):
+        dataBlock = message[4:-1]
+    if len(dataBlock) > 0:
+        patchData = unescapeSysex(dataBlock)
+        return ''.join([chr(x) for x in patchData[107:127]])
+    return "Invalid"
+
 
 def convertToEditBuffer(channel, message):
-	if isEditBufferDump(message):
-		return message
-	elif isSingleProgramDump(message):
-		# Have to strip out bank and program, and set command to edit buffer dump
-		return message[0:3] + [0b00000010] + message[6:]
-	raise "Neither edit buffer nor program dump - can't be converted"
+    if isEditBufferDump(message):
+        return message
+    elif isSingleProgramDump(message):
+        # Have to strip out bank and program, and set command to edit buffer dump
+        return message[0:3] + [0b00000011] + message[6:]
+    raise Exception("Neither edit buffer nor program dump - can't be converted")
+
 
 def unescapeSysex(sysex):
-	result = []
-	dataIndex = 0
-	while (dataIndex < len(sysex)):
-		msbits = sysex[dataIndex]
-		dataIndex += 1
-		for i in range(7):
-			if dataIndex < len(sysex):
-				result.append(sysex[dataIndex] | ((msbits & (1 << i)) << (7 - i)))
-			dataIndex += 1
-	return result
+    result = []
+    dataIndex = 0
+    while dataIndex < len(sysex):
+        msbits = sysex[dataIndex]
+        dataIndex += 1
+        for i in range(7):
+            if dataIndex < len(sysex):
+                result.append(sysex[dataIndex] | ((msbits & (1 << i)) << (7 - i)))
+            dataIndex += 1
+    return result
