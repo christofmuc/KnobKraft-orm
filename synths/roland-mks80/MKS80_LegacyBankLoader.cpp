a149cb36cae70d615dd2c869bae4262511eec504
cruc <christof.ruch@gmx.de>
2020-06-19 00:26:20 +0200

    Fix warning

--- synths/roland-mks80/MKS80_LegacyBankLoader.cpp
+++ synths/roland-mks80/MKS80_LegacyBankLoader.cpp
@@ -9,6 +9,7 @@ namespace midikraft {
 
 	std::string MKS80_LegacyBankLoader::readPascalString(std::vector<uint8>::iterator start, std::vector<uint8>::iterator &position, std::vector<uint8>::iterator const &end)
 	{
+		ignoreUnused(start);
 		std::string name;
 		if (position != end) {
 			// Read length of string
