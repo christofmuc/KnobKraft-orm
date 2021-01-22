b6297aa174cb3e787560d9c1ca6575d14aa2dfa2
Christof <christof.ruch@gmx.de>
2021-01-22 21:31:59 +0100

    Adding missing copyright header

--- The-Orm/ExportDialog.cpp
+++ The-Orm/ExportDialog.cpp
@@ -1,3 +1,9 @@
+/*
+   Copyright (c) 2021 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
 #include "ExportDialog.h"
 
 static std::function<void(midikraft::Librarian::ExportParameters)> sCallback_;
--- The-Orm/ExportDialog.h
+++ The-Orm/ExportDialog.h
@@ -1,3 +1,9 @@
+/*
+   Copyright (c) 2021 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
 #pragma once
 
 #include "JuceHeader.h"
@@ -25,5 +31,4 @@ private:
 	TextButton cancel_;
 
 };
-
 
