8aa75c3347284b2ec0c37b8edcdd6627d56238a1
Christof Ruch <christof.ruch@gmx.de>
2021-05-31 21:56:38 +0200

    Extract the test case written for the Prophet 12, because it can really be extracted and used by all adaptations when we parametrize this correctly. Sadly, doing so in Python is a bit more elaborate.

---
+++ adaptions/sequential/TestAdaptation.py
@@ -0,0 +1,38 @@
+#
+#   Copyright (c) 2021 Christof Ruch. All rights reserved.
+#
+#   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+#
+import unittest
+
+
+#
+# Implement a customizable but generic Test Suite for testing any adaptations functionality!
+#
+
+class AdaptationTestBase(unittest.TestCase):
+
+    def __init__(self, adaptation_module_to_test, example_patch):
+        super().__init__()
+        self.adaptation_module_to_test = adaptation_module_to_test
+        self.example_patch = example_patch
+
+
+class RenameTest(AdaptationTestBase):
+
+    def runTest(self):
+        binary = self.example_patch
+        renamed = self.adaptation_module_to_test.renamePatch(binary, self.adaptation_module_to_test.nameFromDump(binary))
+        self.assertEqual(self.adaptation_module_to_test.calculateFingerprint(renamed),
+                         self.adaptation_module_to_test.calculateFingerprint(binary))
+        new_name = self.adaptation_module_to_test.renamePatch(binary, "newname")
+        result = self.adaptation_module_to_test.nameFromDump(new_name)
+        self.assertEqual(result, "newname")
+
+
+# This is needed to dynamically create the test cases as we parametrize them with the module to test and
+# additional data like an example patch
+def create_tests(adaptation_module_to_test, example_patch):
+    suite = unittest.TestSuite()
+    suite.addTest(RenameTest(adaptation_module_to_test, example_patch))
+    return suite
--- adaptions/sequential/__init__.py
+++ adaptions/sequential/__init__.py
@@ -1,2 +1,2 @@
 from .GenericSequential import GenericSequential
-
+from .TestAdaptation import create_tests
