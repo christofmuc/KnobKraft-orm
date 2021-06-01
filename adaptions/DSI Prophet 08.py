48ff15ef237b6dbc8730ce59df0bcd5604ae1c79
Christof Ruch <christof.ruch@gmx.de>
2021-06-01 22:33:39 +0200

    Move also Prophet 08 and DSI Pro 2 onto the GenericSequential code base.

--- adaptions/DSI Prophet 08.py
+++ adaptions/DSI Prophet 08.py
@@ -1,118 +1,32 @@
 #
-#   Copyright (c) 2020 Christof Ruch. All rights reserved.
+#   Copyright (c) 2021 Christof Ruch. All rights reserved.
 #
 #   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
 #
 
+import sequential
+import sys
 
-def name():
-    return "DSI Prophet 08"  # DSI product name is Prophet '08 (for the year introduced)
+this_module = sys.modules[__name__]
 
+#
+# Configure the GenericSequential module
+#
+synth = sequential.GenericSequential(name="DSI Prophet 08",  # DSI product name is Prophet '08 (for the year introduced)
+                                     device_id=0b00100011,  # See Page 82 of the prophet 12 manual
+                                     banks=2,
+                                     patches_per_bank=128,
+                                     name_len=16,
+                                     name_position=184,
+                                     id_list=[0b00100011, 0b00100100],  # Prophet 08 or special edition Prophet 08
+                                     ).install(this_module)
 
-def createDeviceDetectMessage(channel):
-    # This is a sysex generic device detect message
-    return [0xf0, 0x7e, channel, 0x06, 0x01, 0xf7]
-
-
-def deviceDetectWaitMilliseconds():
-    return 100
-
-
-def needsChannelSpecificDetection():
-    return False
-
-
-def channelIfValidDeviceResponse(message):
-    if (len(message) > 9
-            and message[0] == 0xf0  # Sysex
-            and message[1] == 0x7e  # Non-realtime
-            and message[3] == 0x06  # Device request
-            and message[4] == 0x02  # Device request reply
-            and message[5] == 0x01  # Sequential / Dave Smith Instruments
-            and (message[6] == 0b00100011 or message[6] == 0b00100100)  # Prophet 08 or special edition Prophet 08
-            and message[7] == 0x00
-            and message[8] == 0x00  # Family member 0
-            and message[9] == 0x00):
-        # Extract the current MIDI channel from index 2 of the message
-        return message[2]
-    return -1
-
-
-def createEditBufferRequest(channel):
-    return [0xf0, 0x01, 0b00100011, 0b00000110, 0xf7]
-
-
-def isEditBufferDump(message):
-    return (len(message) > 3
-            and message[0] == 0xf0
-            and message[1] == 0x01  # Sequential
-            and message[2] == 0b00100011  # Prophet 08
-            and message[3] == 0b00000011  # Edit Buffer Data
-            )
-
-
-def numberOfBanks():
-    return 2
-
-
-def numberOfPatchesPerBank():
-    return 128
-
-
-def createProgramDumpRequest(channel, patchNo):
-    bank = patchNo // numberOfPatchesPerBank()
-    program = patchNo % numberOfPatchesPerBank()
-    return [0xf0, 0x01, 0b00100011, 0b00000101, bank, program, 0xf7]
-
-
-def isSingleProgramDump(message):
-    return (len(message) > 3
-            and message[0] == 0xf0
-            and message[1] == 0x01  # Sequential
-            and message[2] == 0b00100011  # Prophet 08
-            and message[3] == 0b00000010  # Program Data
-            )
-
-
-def nameFromDump(message):
-    dataBlock = []
-    if isSingleProgramDump(message):
-        dataBlock = message[6:-1]
-    elif isEditBufferDump(message):
-        dataBlock = message[4:-1]
-    if len(dataBlock) > 0:
-        patchData = unescapeSysex(dataBlock)
-        return ''.join([chr(x) for x in patchData[184:200]])
-    return "Invalid"
-
-
-def convertToEditBuffer(channel, message):
-    if isEditBufferDump(message):
-        return message
-    elif isSingleProgramDump(message):
-        # Have to strip out bank and program, and set command to edit buffer dump
-        return message[0:3] + [0b00000011] + message[6:]
-    raise Exception("Neither edit buffer nor program dump - can't be converted")
-
-
-def convertToProgramDump(channel, message, program_number):
-    bank = program_number // numberOfPatchesPerBank()
-    program = program_number % numberOfPatchesPerBank()
-    if isEditBufferDump(message):
-        return message[0:3] + [0b00000010] + [bank, program] + message[4:]
-    elif isSingleProgramDump(message):
-        return message[0:3] + [0b00000010] + [bank, program] + message[6:]
-    raise Exception("Neither edit buffer nor program dump - can't be converted")
-
-
-def unescapeSysex(sysex):
-    result = []
-    dataIndex = 0
-    while dataIndex < len(sysex):
-        msbits = sysex[dataIndex]
-        dataIndex += 1
-        for i in range(7):
-            if dataIndex < len(sysex):
-                result.append(sysex[dataIndex] | ((msbits & (1 << i)) << (7 - i)))
-            dataIndex += 1
-    return result
+#
+# If this is not loaded as a module, but called as a script, run our unit tests
+#
+if __name__ == "__main__":
+    import unittest
+    messages = sequential.load_sysex("testData/Prophet_08_Programs_v1.0.syx")
+    unittest.TextTestRunner().run(sequential.TestAdaptation.create_tests(this_module,
+                                                                         program_dump=messages[2],
+                                                                         program_name='T8 Strings'))
