256609c9df513c80d67b169b1a60e40c76c1ff75
Christof Ruch <christof.ruch@gmx.de>
2021-06-12 13:59:16 +0200

    Trying debug output

--- adaptions/GenericAdaptation.cpp
+++ adaptions/GenericAdaptation.cpp
@@ -244,7 +244,7 @@ namespace knobkraft {
 		// For Apple (probably for Linux as well?) we need to append the path "python" to the python sys path, so it will find 
 		// python code we are installing, e.g. the generic sequential module which is used by all Sequential synths
 		File pathToMyExecutable = File::getSpecialLocation(File::SpecialLocationType::currentExecutableFile);
-		command = "import sys\nsys.path.append(R\"" + pathToMyExecutable.getChildFile("python").getFullPathName().toStdString() + "\")\n";
+		command = "import sys\nsys.path.append(R\"" + pathToMyExecutable.getChildFile("python").getFullPathName().toStdString() + "\")\nprint(sys.path)\n";
 		py::exec(command);
 #endif
 		checkForPythonOutputAndLog();
