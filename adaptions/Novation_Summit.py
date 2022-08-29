7453435244aa1999a472dcf604da382e976fb790
Christof Ruch <christof.ruch@gmx.de>
2022-08-30 01:00:41 +0200

    [Novation Summit] Adding a first adaptation for the Novation Summit. It might or might not work for the Novation Peak as well. #112

---
+++ adaptions/Novation_Summit.py
@@ -0,0 +1,157 @@
+#   Copyright (c) 2022 Christof Ruch. All rights reserved.
+#   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+#
+#   with the help from https://gearspace.com/board/showpost.php?p=15306436&postcount=1566
+#   and https://gearspace.com/board/electronic-music-instruments-and-electronic-music-production/1149964-novation-peak-96.html
+#   in turn based on Cedric Tessier's Novation Ultranova adaptatio
+import hashlib
+
+NAME_OFFSET = 0x10
+NAME_LEN = 16
+NAME_SPECIALCHARSET = ' !"#$%&\'()*+,-./:;<=>?@[]^_`{|}'
+novation_id = [0x00, 0x20, 0x29]
+summit_id = [0x01, 0x11, 0x01, 0x33]
+peak_id = [0x01, 0x10, 0x00, 0x7e]
+
+
+def name():
+    return 'Novation Summit'
+
+
+def createDeviceDetectMessage(channel):
+    return [0xf0, 0x7e, 0x7f, 0x06, 0x01, 0xf7]
+
+
+def deviceDetectWaitMilliseconds():
+    return 200
+
+
+def needsChannelSpecificDetection():
+    return False
+
+
+def channelIfValidDeviceResponse(message):
+    if (len(message) > 9
+            and message[0] == 0xf0  # Sysex
+            and message[1] == 0x7e
+            and message[2] == 0x7f
+            and message[3] == 0x06
+            and message[4] == 0x02
+            and message[5:8] == novation_id
+            and message[8] == 0x33  # This ought to be the Summit
+            and message[9] == 0x01):
+        # From that reply we cannot find out on which MIDI channel/Device ID the Summit is setup
+        return 1
+    return -1
+
+
+def bankDescriptors():
+    return [{"bank": x, "name": f"Bank {chr(ord('A')+x)}", "size": 128, "type": "Single Patch"} for x in range(4)]
+
+
+def createEditBufferRequest(channel):
+    return [0xf0] + novation_id + summit_id + [0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf7]
+
+
+def isEditBufferDump(message):
+    return isOwnSysex(message) and len(message) > 8 and message[8] == 0x00  # Single patch to edit buffer
+
+
+def isOwnSysex(message):
+    return (len(message) > 8
+            and message[0] == 0xf0
+            and message[1:4] == novation_id
+            and (message[4:8] == summit_id or message[4:8] == peak_id)
+            )
+
+
+def createProgramDumpRequest(channel, patchNo):
+    bank = patchNo // 182
+    program = patchNo % 128
+    return [0xf0] + novation_id + summit_id + [0x41, 0x00, 0x00, 0x00, bank, program, 0xf7]
+
+
+def isSingleProgramDump(message):
+    return isOwnSysex(message) and len(message) > 8 and message[8] == 0x01  # Single patch send command
+
+
+def numberFromDump(message):
+    if not isSingleProgramDump(message):
+        return 0
+    bank = message[12] % 4  # Not more than 4 banks allowed
+    program = message[13] % 128  # Maximum 128 programs per bank
+    return bank * 128 + program
+
+
+def nameFromDump(message):
+    if isSingleProgramDump(message) or isEditBufferDump(message):
+        name = ''.join([chr(x) for x in message[0x10:0x10+16]])
+        return name.strip()
+    return 'Invalid'
+
+
+def convertToEditBuffer(channel, message):
+    if isEditBufferDump(message):
+        return message
+    elif isSingleProgramDump(message):
+        return message[:8] + [0x00, 0x00, 0x00, 0x00, 0x00, 0x00] + message[0x0e:]
+    raise Exception("Neither edit buffer nor program dump - can't be converted")
+
+
+def convertToProgramDump(channel, message, program_number):
+    bank = program_number // 128
+    program = program_number % 128
+    if isEditBufferDump(message) or isSingleProgramDump(message):
+        return message[:8] + [0x01, 0x00, 0x00, 0x00, bank, program] + message[0x0e:]
+    raise Exception("Neither edit buffer nor program dump - can't be converted")
+
+
+def renamePatch(message, new_name):
+    if isSingleProgramDump(message) or isEditBufferDump(message):
+        # name is alpha-num + special chars (replace everything else by '_' and pad with spaces)
+        clean_name = new_name.strip()[:NAME_LEN].ljust(NAME_LEN, ' ')
+        valid_name = [ord(x) if x.isalnum() or x in NAME_SPECIALCHARSET else ord('_') for x in clean_name]
+        return message[:NAME_OFFSET] + valid_name + message[NAME_OFFSET+NAME_LEN:]
+    raise Exception("Neither edit buffer nor program dump can't be converted")
+
+
+def bankName(bank):
+    return f"Bank {chr(ord('A')+(bank % 4))}"
+
+
+def friendlyProgramName(program):
+    return "%s%d" % (bankName(program // 128), program % 128)
+
+
+def calculateFingerprint(message):
+    # Only fingerprint data outside of the header, beyond the patch name
+    data = message[NAME_OFFSET+NAME_LEN:-1]
+    return hashlib.md5(bytearray(data)).hexdigest()
+
+
+def run_tests():
+    with open("testData/NovationPeak-Reflections.syx", "rb") as sysex:
+        raw_data = list(sysex.read())
+        assert isEditBufferDump(raw_data)
+        assert numberFromDump(raw_data) == 0
+
+        buffer = convertToEditBuffer(0, raw_data)
+        assert isEditBufferDump(buffer)
+        assert numberFromDump(buffer) == 0
+
+        back_dump = convertToProgramDump(1, buffer, 130)
+        assert numberFromDump(back_dump) == 130
+
+        assert nameFromDump(raw_data) == "Reflections"
+        assert nameFromDump(buffer) == nameFromDump(back_dump)
+        same_patch = renamePatch(raw_data, "Different")
+        assert nameFromDump(same_patch) == "Different"
+        same_same = renamePatch(same_patch, "Cr4zy Name°$    overflow")
+        assert nameFromDump(same_same) == "Cr4zy Name_$"
+
+        assert calculateFingerprint(same_same) == calculateFingerprint(raw_data)
+        assert bankName(2) == 'Bank C'
+
+
+if __name__ == "__main__":
+    run_tests()
