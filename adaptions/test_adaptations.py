5a43eba54ffbab13aef8ab9393acaafa4b13ddbb
Christof Ruch <christof.ruch@gmx.de>
2023-06-18 00:32:44 +0200

    [testing] Fix tests for adaptations that have no nameFromDump()

--- adaptions/test_adaptations.py
+++ adaptions/test_adaptations.py
@@ -241,7 +241,8 @@ def test_convert_programs_to_programs(adaptation, test_data: testing.TestData):
         pytest.skip(f"Skipping program to program conversion test for {adaptation.name()} as it indicates this conversion is not idempotent")
 
     for program_buffer in test_data.programs:
-        original_name = adaptation.nameFromDump(program_buffer.message.byte_list)
+        if hasattr(adaptation, "nameFromDump"):
+            original_name = adaptation.nameFromDump(program_buffer.message.byte_list)
         target_no = get_target_program_no(program_buffer)
         rebuild_program = adaptation.convertToProgramDump(0x00, program_buffer.message.byte_list, target_no)
 
