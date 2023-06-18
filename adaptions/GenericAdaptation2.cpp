85ea3a553acc867b567c9708b914ff3c1e85ba4b
Christof Ruch <christof.ruch@gmx.de>
2023-06-18 18:17:05 +0200

    [adaptation 2.0] Testing the validity of just exposing the C++ classes to the Python interface, and implementing the Python adaptations using native C++ classes instead of abstracting everything away. This seems to use much less boiler plate code than before, even if there is quite a lot, and it solves the type confusion on the Python interface with the single message or list of message or what not.

---
+++ adaptions/GenericAdaptation2.cpp
@@ -0,0 +1,330 @@
+#include "Synth.h"
+#include "Patch.h"
+
+#include "EditBufferCapability.h"
+#include "HasBanksCapability.h"
+
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+using namespace py::literals;
+using namespace midikraft;
+
+
+// Trampoline class to allow Python to override C++ virtual functions
+// See https://pybind11.readthedocs.io/en/stable/advanced/classes.html
+//
+class PySynth : public Synth
+{
+public:
+	using Synth::Synth;
+
+	virtual std::string getName() const override
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			std::string,
+			NamedDeviceCapability,
+			"get_name",
+			getName,
+			);
+	}
+
+	virtual std::shared_ptr<DataFile> patchFromPatchData(const Synth::PatchData& data, MidiProgramNumber place) const override
+	{
+		ignoreUnused(place);
+		return std::make_shared<DataFile>(0, data);
+	}
+
+	// TODO this should go away
+	virtual bool isOwnSysex(MidiMessage const& message) const override
+	{
+		ignoreUnused(message);
+		return false;
+	}
+
+	virtual std::string friendlyProgramAndBankName(MidiBankNumber bankNo, MidiProgramNumber programNo) const override {
+		PYBIND11_OVERRIDE_NAME(
+			std::string, // Return type
+			Synth, // Parent class
+			"friendly_program_name", // Python function name
+			friendlyProgramAndBankName, // C++ method name
+			bankNo,
+			programNo
+		);
+	}
+
+	// Override this if you disagree with the default implementation of calculating the fingerprint with an md5 of the filtered patch data
+	virtual std::string calculateFingerprint(std::shared_ptr<DataFile> patch) const override
+	{
+		PYBIND11_OVERRIDE_NAME(
+			std::string,
+			Synth,
+			"calculate_fingerprint",
+			calculateFingerprint,
+			patch
+		);
+	}
+
+	// Override this if you have some words for the user of this synth to properly do the manual setup steps that might be required for vintage gear
+	virtual std::string setupHelpText() const override {
+		PYBIND11_OVERRIDE_NAME(
+			std::string,
+			Synth,
+			"setup_help_text",
+			setupHelpText
+		);
+	}
+};
+
+class PyDiscoverableDevice : public DiscoverableDevice {
+public:
+	using DiscoverableDevice::DiscoverableDevice;
+
+	virtual std::vector<juce::MidiMessage> deviceDetect(int channel) override
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			std::vector<juce::MidiMessage>,
+			DiscoverableDevice,
+			"create_device_detect_message",
+			deviceDetect,
+			channel
+		);
+	}
+
+	virtual int deviceDetectSleepMS() override
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			int,
+			DiscoverableDevice,
+			"device_detect_sleep_ms",
+			deviceDetectSleepMS,
+			);
+	}
+
+	virtual MidiChannel channelIfValidDeviceResponse(const juce::MidiMessage& message) override
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			MidiChannel,
+			DiscoverableDevice,
+			"channel_if_valid_device_detect_response",
+			channelIfValidDeviceResponse,
+			message
+		);
+	}
+
+	virtual bool needsChannelSpecificDetection()
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			bool,
+			DiscoverableDevice,
+			"needs_channel_specific_detection",
+			needsChannelSpecificDetection,
+			);
+	}
+
+	virtual bool endDeviceDetect(MidiMessage& endMessageOut) const
+	{
+		return false;
+	}
+
+	virtual bool wasDetected() const
+	{
+		return wasDetected_;
+	}
+
+	virtual void setWasDetected(bool wasDetected)
+	{
+		wasDetected_ = wasDetected;
+	}
+
+private:
+	bool wasDetected_ = false;
+};
+
+class PyEditBufferCapability : public EditBufferCapability
+{
+public:
+	using EditBufferCapability::EditBufferCapability;
+
+	virtual std::vector<MidiMessage> requestEditBufferDump() const override
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			std::vector<MidiMessage>,
+			EditBufferCapability,
+			"request_edit_buffer",
+			requestEditBufferDump,
+			);
+	}
+
+	virtual bool isEditBufferDump(const std::vector<MidiMessage>& messages) const override
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			bool,
+			EditBufferCapability,
+			"is_edit_buffer",
+			isEditBufferDump,
+			messages
+		);
+
+	}
+
+	virtual HandshakeReply isMessagePartOfEditBuffer(const MidiMessage& message) const override {
+		PYBIND11_OVERRIDE_NAME(
+			HandshakeReply,
+			EditBufferCapability,
+			"is_part_of_edit_buffer",
+			isMessagePartOfEditBuffer,
+			message
+		);
+	}
+
+	virtual std::shared_ptr<DataFile> patchFromSysex(const std::vector<MidiMessage>& message) const override
+	{
+		py::gil_scoped_acquire acquire;
+		// For the Generic Adaptation, this is a nop, as we do not unpack the MidiMessage, but rather store the raw MidiMessage
+		midikraft::Synth::PatchData data;
+		for (auto const& m : message) {
+			std::copy(m.getRawData(), m.getRawData() + m.getRawDataSize(), std::back_inserter(data));
+		}
+		return std::make_shared<DataFile>(0, data);
+	}
+
+	virtual std::vector<MidiMessage> patchToSysex(std::shared_ptr<DataFile> patch) const override
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			std::vector<MidiMessage>,
+			EditBufferCapability,
+			"convert_to_edit_buffer",
+			patchToSysex,
+			patch
+		);
+	}
+
+	virtual MidiMessage saveEditBufferToProgram(int programNumber) override
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			MidiMessage,
+			EditBufferCapability,
+			"save_edit_buffer",
+			saveEditBufferToProgram,
+			programNumber
+		);
+	}
+
+};
+
+class PyHasBankDescriptorsCapability : public HasBankDescriptorsCapability
+{
+public:
+	using HasBankDescriptorsCapability::HasBankDescriptorsCapability;
+
+	virtual std::vector<BankDescriptor> bankDescriptors() const override
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			std::vector<BankDescriptor>,
+			HasBankDescriptorsCapability,
+			"bank_descriptors",
+			bankDescriptors,
+		);
+	}
+
+	virtual std::vector<juce::MidiMessage> bankSelectMessages(MidiBankNumber bankNo) const
+	{
+		PYBIND11_OVERRIDE_PURE_NAME(
+			std::vector<juce::MidiMessage>,
+			HasBankDescriptorsCapability,
+			"bank_select",
+			bankSelectMessages,
+			bankNo
+		);
+	}
+};
+
+juce::MidiMessage midiMesssageFromBytes(std::vector<uint8_t> const& content)
+{
+	return juce::MidiMessage(content.data(), content.size());
+}
+
+MidiBankNumber bankFromZeroBasedInt(int bank, int bankSize)
+{
+	return MidiBankNumber::fromZeroBase(bank, bankSize);
+}
+
+MidiProgramNumber programFromZeroBasedInt(MidiBankNumber bank, int program)
+{
+	return MidiProgramNumber::fromZeroBaseWithBank(bank, program);
+}
+
+
+PYBIND11_MODULE(orm_synth, m)
+{
+	py::class_<juce::MidiMessage>(m, "MidiMessage")
+		.def(py::init(&midiMesssageFromBytes))
+		.def("__repr__", [](MidiMessage const& message) { return "<" + message.getDescription().toStdString() + ">";  })
+		.def("__len__", [](MidiMessage const& message) { return message.getRawDataSize(); })
+		.def("__getitem__", [](MidiMessage const& message, int index) { return index < message.getRawDataSize() ? message.getRawData()[index] : 0;  })
+		.def("__getitem__", [](MidiMessage const& message, py::slice slice) {
+		py::ssize_t start, stop, step, slicelength;
+		if (!slice.compute(message.getRawDataSize(), &start, &stop, &step, &slicelength)) {
+			throw py::error_already_set();
+		}
+		std::vector<int> result;
+		for (py::ssize_t i = 0; i < slicelength; ++i) {
+			result.push_back(message.getRawData()[start]);
+			start += step;
+		}
+		return result;
+			});
+
+	py::class_<MidiBankNumber>(m, "Bank")
+		.def(py::init(&bankFromZeroBasedInt))
+		.def_static("from_zero_base", &MidiBankNumber::fromZeroBase)
+		.def_static("from_one_base", &MidiBankNumber::fromOneBase)
+		.def_static("invalid", &MidiBankNumber::invalid)
+		.def("is_valid", &MidiBankNumber::isValid)
+		.def("size", &MidiBankNumber::bankSize)
+		.def("to_zero_base", &MidiBankNumber::toZeroBased)
+		.def("to_one_base", &MidiBankNumber::toOneBased);
+
+	py::class_<MidiProgramNumber>(m, "Program")
+		.def(py::init(&programFromZeroBasedInt))
+		.def("value", &MidiProgramNumber::toZeroBasedWithBank)
+		.def_static("from_zero_base", &MidiProgramNumber::fromZeroBase)
+		.def_static("from_one_base", &MidiProgramNumber::fromOneBase);
+
+	py::class_<BankDescriptor>(m, "BankDescriptor")
+		.def(py::init<MidiBankNumber, std::string, int, bool, std::string>(),
+			py::arg("bank"), py::arg("name"), py::arg("size"), py::arg("is_rom"), py::arg("type"))
+		.def_readwrite("bank", &BankDescriptor::bank)
+		.def_readwrite("name", &BankDescriptor::name)
+		.def_readwrite("size", &BankDescriptor::size)
+		.def_readwrite("is_rom", &BankDescriptor::isROM)
+		.def_readwrite("type", &BankDescriptor::type);
+
+	py::class_<Synth, PySynth>(m, "Synth")
+		.def(py::init<>())
+		.def("get_name", &Synth::getName)
+		.def("friendly_program_name", &Synth::friendlyProgramAndBankName)
+		.def("calculate_fingerprint", &Synth::calculateFingerprint)
+		.def("setup_help_text", &Synth::setupHelpText);
+
+	py::class_<DiscoverableDevice, PyDiscoverableDevice>(m, "DiscoverableDevice")
+		.def(py::init<>())
+		.def("create_device_detect_message", &DiscoverableDevice::deviceDetect)
+		.def("device_detect_sleep_ms", &DiscoverableDevice::deviceDetectSleepMS)
+		.def("channel_if_valid_device_detect_response", &DiscoverableDevice::channelIfValidDeviceResponse)
+		.def("needs_channel_specific_detection", &DiscoverableDevice::needsChannelSpecificDetection);
+
+	py::class_<EditBufferCapability, PyEditBufferCapability>(m, "EditBufferCapability")
+		.def(py::init<>())
+		.def("request_edit_buffer", &EditBufferCapability::requestEditBufferDump)
+		.def("is_edit_buffer", &EditBufferCapability::isEditBufferDump)
+		.def("is_part_of_edit_buffer", &EditBufferCapability::isMessagePartOfEditBuffer)
+		.def("convert_to_edit_buffer", &EditBufferCapability::patchToSysex)
+		.def("save_edit_buffer", &EditBufferCapability::saveEditBufferToProgram);
+
+	py::class_<HasBankDescriptorsCapability, PyHasBankDescriptorsCapability>(m, "BankDescriptorsCapability")
+		.def(py::init<>())
+		.def("bank_descriptors", &HasBankDescriptorsCapability::bankDescriptors)
+		.def("bank_select", &HasBankDescriptorsCapability::bankSelectMessages);
+}
--- adaptions/KorgDW8000.py
+++ adaptions/KorgDW8000.py
@@ -1,73 +1,72 @@
 #
-#   Copyright (c) 2020 Christof Ruch. All rights reserved.
+#   Copyright (c) 2023 Christof Ruch. All rights reserved.
 #
 #   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
 #
 from typing import List
 
 import testing
+from orm_synth import *
 
 
-def name():
-    return "Korg DW-8000 Adaptation"
+class Korg_DW8000(Synth, DiscoverableDevice, EditBufferCapability, BankDescriptorsCapability):
 
+    def __init__(self):
+        Synth.__init__(self)
+        DiscoverableDevice.__init__(self)
+        EditBufferCapability.__init__(self)
+        BankDescriptorsCapability.__init__(self)
+        self.channel = 0
 
-def createDeviceDetectMessage(channel):
-    # Page 5 of the service manual - Device ID Request
-    return [0xf0, 0x42, 0x40 | (channel % 16), 0xf7]
+    def get_name(self):
+        return "Korg DW-8000 Adaptation"
 
+    def bank_descriptors(self) -> List[BankDescriptor]:
+        return [BankDescriptor(bank=Bank(0, 64), name="Internal", size=64, type="Patch", is_rom=False)]
 
-def deviceDetectWaitMilliseconds():
-    return 100
+    def friendly_program_name(self, bank: Bank, program: Program) -> str:
+        return "%d%d" % (program.value() // 8 + 1, (program.value() % 8) + 1)
 
+    def needs_channel_specific_detection(self) -> bool:
+        return True
 
-def needsChannelSpecificDetection():
-    return True
+    def create_device_detect_message(self, channel) -> List[MidiMessage]:
+        # Page 5 of the service manual - Device ID Request
+        return [MidiMessage([0xf0, 0x42, 0x40 | (channel % 16), 0xf7])]
 
+    def channel_if_valid_device_detect_response(self, message: MidiMessage) -> int:
+        # Page 3 of the service manual - Device ID
+        if (len(message) > 3
+                and message[0] == 0xf0  # Sysex
+                and message[1] == 0x42  # Korg
+                and (message[2] & 0xf0) == 0x30  # Device ID
+                and message[3] == 0x03):  # DW-8000
+            self.channel = message[2] & 0x0f
+            return self.channel
+        return -1
 
-def channelIfValidDeviceResponse(message):
-    # Page 3 of the service manual - Device ID
-    if (len(message) > 3
-            and message[0] == 0xf0  # Sysex
-            and message[1] == 0x42  # Korg
-            and (message[2] & 0xf0) == 0x30  # Device ID
-            and message[3] == 0x03):  # DW-8000
-        return message[2] & 0x0f
-    return -1
+    def device_detect_sleep_ms(self) -> int:
+        return 100
 
+    def request_edit_buffer(self) -> List[MidiMessage]:
+        # Page 5 - Data Save Request
+        return [MidiMessage([0xf0, 0x42, 0x30 | self.channel, 0x03, 0x10, 0xf7])]
 
-def createEditBufferRequest(channel):
-    # Page 5 - Data Save Request
-    return [0xf0, 0x42, 0x30 | (channel % 16), 0x03, 0x10, 0xf7]
+    def is_edit_buffer(self, messages: List[MidiMessage]) -> bool:
+        # Page 3 - Data Dump
+        message = messages[0]
+        return (len(message) > 4
+                and message[0] == 0xf0
+                and message[1] == 0x42  # Korg
+                and (message[2] & 0xf0) == 0x30  # Format, ignore MIDI Channel in lower 4 bits
+                and message[3] == 0x03  # DW-8000
+                and message[4] == 0x40  # Data Dump
+                )
 
-
-def isEditBufferDump(message):
-    # Page 3 - Data Dump
-    return (len(message) > 4
-            and message[0] == 0xf0
-            and message[1] == 0x42  # Korg
-            and (message[2] & 0xf0) == 0x30  # Format, ignore MIDI Channel in lower 4 bits
-            and message[3] == 0x03  # DW-8000
-            and message[4] == 0x40  # Data Dump
-            )
-
-
-def numberOfBanks():
-    return 1
-
-
-def numberOfPatchesPerBank():
-    return 64
-
-
-def convertToEditBuffer(channel, message):
-    if isEditBufferDump(message):
-        return message[0:2] + [0x30 | channel] + message[3:]
-    raise Exception("This is not an edit buffer - can't be converted")
-
-
-def friendlyProgramName(program):
-    return "%d%d" % (program // 8 + 1, (program % 8) + 1)
+    def convert_to_edit_buffer(self, patch: List[MidiMessage]) -> List[MidiMessage]:
+        if self.is_edit_buffer(patch):
+            return [MidiMessage(patch[0][0:2] + [0x30 | self.channel] + patch[0][3:])]
+        raise Exception("This is not an edit buffer - can't be converted")
 
 
 def make_test_data():
@@ -75,5 +74,13 @@ def make_test_data():
         patch = [x for sublist in data.all_messages[0:2] for x in sublist]
         yield testing.ProgramTestData(message=patch)
 
-    assert friendlyProgramName(0) == "11"
+    # assert friendlyProgramName(0) == "11"
     return testing.TestData(sysex="testData/KorgDW8000_bank_a.syx", edit_buffer_generator=programs, friendly_bank_name=(63, "88"))
+
+
+if __name__ == "__main__":
+    korg = Korg_DW8000()
+    print(korg.get_name())
+    print(korg.friendly_program_name(Bank(0, 64), Program.from_zero_base(17)))
+    print(korg.create_device_detect_message(3))
+    print(korg.bank_descriptors()[0].name)
