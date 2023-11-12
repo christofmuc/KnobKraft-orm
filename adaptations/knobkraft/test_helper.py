575e6133c90e5c85d47090594b4e2124d4eb4022
Christof Ruch <christof.ruch@gmx.de>
2023-11-12 23:44:04 +0100

    [Adaptation testing] When comparing lists, always compare them to the end and only then fail
    (cherry picked from commit f83c66b91eea0b3b85f682dbdece604093971ad7)

--- adaptations/knobkraft/test_helper.py
+++ adaptations/knobkraft/test_helper.py
@@ -7,12 +7,13 @@ def list_compare(list1: List, list2: List) -> bool:
         print(f"list2: {list2}")
         print(f"Lists differ in lengths: {len(list1)} != {len(list2)}")
         return False
+    result = True
     for i in range(len(list1)):
         if list1[i] != list2[i]:
             print(f"\nlist1: {list1}")
             print(f"list2: {list2}")
             print(f"Result differ at position {i}: {list1[i]} vs {list2[i]}")
-            return False
-    return True
+            result = False
+    return result
 
 
