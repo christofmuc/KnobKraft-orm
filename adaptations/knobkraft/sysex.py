227898223ce37757141483bf3a4c9d3738e7f150
Christof Ruch <christof.ruch@gmx.de>
2024-01-17 23:49:58 +0100

    [RefaceDX] Implement the new functions for the reface DX - bank storage is now possible!
    (cherry picked from commit dd3ad2e5ca258970d4390d038ebd0d6b1fdd23e8)

--- adaptations/knobkraft/sysex.py
+++ adaptations/knobkraft/sysex.py
@@ -83,4 +83,17 @@ def unescapeSysex_deepmind(sysex):
     return result
 
 
+def denibble_hi_then_lo(message):
+    return [message[x+1] | (message[x] << 4) for x in range(0, len(message), 2)]
 
+
+def denibble_lo_then_hi(message):
+    return [message[x] | (message[x + 1] << 4) for x in range(0, len(message), 2)]
+
+
+def nibble(message):
+    result = []
+    for b in message:
+        result.append(b & 0x0f)
+        result.append((b & 0xf0) >> 4)
+    return result
