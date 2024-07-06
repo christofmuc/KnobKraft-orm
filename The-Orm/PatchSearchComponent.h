e3e806cdb9d2e700e55aa851fc4722c45b5e6bdf
Christof <christof.ruch@gmx.de>
2024-07-06 17:33:28 +0200

    [UI] Add a combo box allowing to select one of 4 sort orders: By import (the original), by name, by program no, or by bank no. This is #39.

--- The-Orm/PatchSearchComponent.h
+++ The-Orm/PatchSearchComponent.h
@@ -50,8 +50,9 @@ private:
 	ToggleButton onlyUntagged_;
 	ToggleButton onlyDuplicates_;
 	ToggleButton andCategories_;
-	ComboBox buttonDisplayType_;
 	TextButton clearFilters_;
+	ComboBox orderByType_;
+	ComboBox buttonDisplayType_;
 
 	midikraft::PatchDatabase& database_;
 };
