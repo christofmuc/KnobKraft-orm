81c367ed564d7d08d70f9b58303f0b27521af324
Christof <christof.ruch@gmx.de>
2024-08-02 00:26:55 +0200

    [refaceDX] Simplify the code that calculates the checksum, makes it match the specification better #324.

--- adaptations/YamahaRefaceDX.py
+++ adaptations/YamahaRefaceDX.py
@@ -304,15 +304,12 @@ def dataBlockFromMessage(message):
     if isOwnSysex(message):
         data_len = message[5] << 7 | message[6]
         if len(message) == data_len + 9:
-            data_block = message[8:-2]
-            check_sum = -0x05
-            for d in data_block:
-                check_sum -= d
-            if (check_sum & 0x7f) == message[-2]:
-                # Check sum passed
-                return data_block
-            else:
-                raise Exception("Checksum error in reface DX data")
+            # The Check-sum is the value that results in a value of 0 for the
+            # lower 7 bits when the Model ID, Start Address, Data and Check sum itself are added.
+            checksum_block = message[0x07:-1]
+            if (sum(checksum_block) & 0x7f) == 0:
+                # return "Data" block
+                return message[0x08:-2]
     raise Exception("Got corrupt data block in refaceDX data")
 
 
