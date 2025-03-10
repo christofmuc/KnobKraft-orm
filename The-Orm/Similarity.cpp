6a0753ae57e85815bbfe3ebe8f19c59213cee3ef
Christof <christof.ruch@gmx.de>
2025-03-10 21:38:19 +0100

    Add a progress dialog while calculating the index

--- The-Orm/Similarity.cpp
+++ The-Orm/Similarity.cpp
@@ -2,6 +2,7 @@
 
 #include "Capability.h"
 #include "DetailedParametersCapability.h"
+#include "ProgressHandlerWindow.h"
 
 #include "faiss/IndexFlat.h"
 
@@ -14,49 +15,58 @@ struct SearchIndex {
 	std::shared_ptr<faiss::Index> index;
 };
 
+typedef std::function<void(midikraft::PatchHolder const& patch, float* position, size_t& d)> PatchFeatureVector;
+
 class ExactSimilaritySearch {
 public:
-	typedef std::function<void(midikraft::PatchHolder const& patch, float* position, size_t &d)> PatchFeatureVector;
 
 	ExactSimilaritySearch(midikraft::PatchDatabase& db) : db_(db) {
 	}
 
-
 	~ExactSimilaritySearch() = default;
 
-	void buildIndexForSynth(std::shared_ptr<midikraft::Synth> synth, PatchFeatureVector featureVector) {
-		auto index = indexes_.find(synth->getName());
-		if (index == indexes_.end()) {
-			// Load all patches from the database for now
-			midikraft::PatchFilter filter({synth});
-			auto patches = db_.getPatches(filter, 0, -1);
+	void buildIndexForSynth(std::shared_ptr<midikraft::Synth> synth, PatchFeatureVector featureVector, ProgressHandler *progressHandler) {
+		progressHandler->setProgressPercentage(0.0);
+		progressHandler->setMessage("Loading all patches into memory...");
+		// Load all patches from the database for now
+		midikraft::PatchFilter filter({synth});
+		auto patches = db_.getPatches(filter, 0, -1);
 
-			// Now, we build two indices: The first is just integer to md5,
-			// the second is the vector index of dimensionality equal to patch size
-			// In this instance, we are just using the bytes (non blanked) of the patch
-			// so a different name will already make a big difference.
-			auto id_map = std::make_shared<std::map<size_t, std::string>>();
-			size_t dimensionality = 0;
-			for (size_t i = 0; i < patches.size(); i++) {
-				id_map->emplace(i, patches[i].md5());
-				if (dimensionality == 0) {
-					// Need to calculate a feature vector to glean the dimensionality
-					featureVector(patches[i], nullptr, dimensionality);
-				}
-				dimensionality = std::max(dimensionality, patches[i].patch()->data().size());
-			}
-
-			auto newIndex = std::make_shared<faiss::IndexFlatL2>(dimensionality);
-			// This matrix needs a lot of memory
-			float* xb = new float[dimensionality * patches.size()];
-			size_t write_pos = 0;
-			for (auto const& patch : patches) {
-				featureVector(patch, &xb[write_pos], dimensionality);
-				write_pos += dimensionality;
-			}
-			newIndex->add(patches.size(), xb);
-			indexes_.emplace(synth->getName(), SearchIndex{ dimensionality, patches.size(), id_map, newIndex});
+		if (patches.size() == 0) {
+			spdlog::error("No patches loaded from database, can't build similarity index!");
+			return;
 		}
+
+		// Need to calculate a feature vector to glean the dimensionality
+		size_t dimensionality = 0;
+		featureVector(patches[0], nullptr, dimensionality);
+
+		// Now, we build two indices: The first is just integer to md5,
+		// the second is the vector index of dimensionality equal to patch size
+		// In this instance, we are just using the bytes (non blanked) of the patch
+		// so a different name will already make a big difference.
+		auto id_map = std::make_shared<std::map<size_t, std::string>>();
+		progressHandler->setMessage("Calculating feature vectors for patches...");
+		auto newIndex = std::make_shared<faiss::IndexFlatL2>(dimensionality);
+		// This matrix needs a lot of memory
+		float* xb = new float[dimensionality * patches.size()];
+		size_t write_pos = 0;
+		for (size_t i = 0; i < patches.size(); i++) {
+			id_map->emplace(i, patches[i].md5());
+			featureVector(patches[i], &xb[write_pos], dimensionality);
+			write_pos += dimensionality;			
+			progressHandler->setProgressPercentage(i / static_cast<double>(patches.size()));
+			if (progressHandler->shouldAbort()) {
+				spdlog::info("Calculation of similarity index interrupted");
+				return;
+			}
+		}
+		newIndex->add(patches.size(), xb);
+		indexes_.emplace(synth->getName(), SearchIndex{ dimensionality, patches.size(), id_map, newIndex});
+	}
+
+	bool hasIndex(std::shared_ptr<midikraft::Synth> synth) {
+		return indexes_.find(synth->getName()) != indexes_.end();
 	}
 
 	std::vector<std::string> searchNeighbours(midikraft::PatchHolder const& examplePatch, int k, PatchFeatureVector featureVector) {
@@ -121,10 +131,36 @@ void featuresFromParameters(midikraft::PatchHolder const& patch, float* matrix, 
 	}
 }
 
+class BuildFeatureIndexWindow : public ProgressHandlerWindow {
+public:
+	BuildFeatureIndexWindow(std::shared_ptr<midikraft::Synth> synth
+		, ExactSimilaritySearch &search
+		, PatchFeatureVector featureVector
+	) 
+		: ProgressHandlerWindow("Creating index", fmt::format("Building the in-memory index for all patches of the {}", synth->getName())),
+		synth_(synth), search_(search), featureVector_(featureVector)
+	{
+	}
+
+	virtual void run() override {
+		search_.buildIndexForSynth(synth_, featureVector_, this);
+	}
+
+private:
+	std::shared_ptr<midikraft::Synth> synth_;
+	ExactSimilaritySearch& search_;
+	PatchFeatureVector featureVector_;
+};
 
 std::vector<midikraft::PatchHolder> PatchSimilarity::findSimilarPatches(midikraft::PatchHolder const& examplePatch, int k) {
-	// Now, we need to build an index of all patches for this synth, should it not exist yet
-	impl_->buildIndexForSynth(examplePatch.smartSynth(), featuresFromParameters);
+	if (!impl_->hasIndex(examplePatch.smartSynth())) {
+		// Now, we need to build an index of all patches for this synth, should it not exist yet
+		BuildFeatureIndexWindow progressWindow(examplePatch.smartSynth(), *impl_, featuresFromParameters);
+		if (!progressWindow.runThread()) {
+			// User abort
+			return {};
+		}
+	}
 
 	// And then call search
 	auto neighbours = impl_->searchNeighbours(examplePatch, k, featuresFromParameters);
