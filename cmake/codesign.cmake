cdcd2b68b4a26af8ff026ebe2828a1edeba1f1ba
Christof <christof.ruch@gmx.de>
2025-11-03 00:34:44 +0100

    Trying to fix sparkle build

--- cmake/codesign.cmake
+++ cmake/codesign.cmake
@@ -26,9 +26,11 @@ endforeach()
 # Now fixup our linking and executable paths
 include(BundleUtilities)
 set(BU_CHMOD_BUNDLE_ITEMS TRUE)
+# Provide Sparkle in the search path so fixup_bundle can resolve @rpath references.
 # We need the IGNORE Python because of https://gitlab.kitware.com/cmake/cmake/-/issues/20165
 # Patching the bundle utils could fix it: https://stackoverflow.com/questions/59415784/cmake-macos-bundleutilities-adds-python-interpreter-to-app-and-doesnt-do-fi
-fixup_bundle("${SIGN_DIRECTORY}"  ""  "" IGNORE_ITEM "Python")
+set(FIXUP_SEARCH_DIRS "${SIGN_DIRECTORY}/Contents/Frameworks")
+fixup_bundle("${SIGN_DIRECTORY}"  ""  "${FIXUP_SEARCH_DIRS}" IGNORE_ITEM "Python")
 
 # Lastly, sign our executable
 message(STATUS "Signing with '${CODESIGN_CERTIFICATE_NAME}'")
