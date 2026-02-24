151eefa74084d85246d7bb1a1fd2bccfdf21470d
Christof <christof.ruch@gmx.de>
2026-02-25 00:09:08 +0100

    Harden the hdiutil script

--- The-Orm/hdiutil_repeat.sh
+++ The-Orm/hdiutil_repeat.sh
@@ -4,20 +4,42 @@
 
 set -e
 
-if [ "$1" != "create" ]; then
-  # If it isn't an `hdiutil create` command, just run and exit normally
-  hdiutil "$@"
+cmd="$1"
+
+retry_hdiutil() {
+  local max_retries="$1"
+  shift
+  local i=0
+  until hdiutil "$@"
+  do
+    if [ "$i" -eq "$max_retries" ]; then
+      return 1
+    fi
+    i=$((i+1))
+    sleep 1
+  done
+}
+
+if [ "$cmd" = "create" ]; then
+  # For an `hdiutil create` command, try repeatedly, up to 10 times.
+  # This prevents spurious errors caused by a race condition with XProtect.
+  # See https://github.com/actions/runner-images/issues/7522
+  retry_hdiutil 10 "$@"
   exit 0
 fi
 
-# For an `hdiutil create` command, try repeatedly, up to 10 times
-# This prevents spurious errors caused by a race condition with XProtect
-# See https://github.com/actions/runner-images/issues/7522
-i=0
-until
+if [ "$cmd" = "detach" ]; then
+  # DMG detach can intermittently fail in CI due to temporary volume busy states.
+  if retry_hdiutil 10 "$@"; then
+    exit 0
+  fi
+
+  # Last-resort fallback for stale/busy mounts.
+  if [ -n "${2:-}" ]; then
+    hdiutil detach -force "$2"
+    exit 0
+  fi
+fi
+
+# For all other commands, just run once.
 hdiutil "$@"
-do
-if [ $i -eq 10 ]; then exit 1; fi
-i=$((i+1))
-sleep 1
-done
